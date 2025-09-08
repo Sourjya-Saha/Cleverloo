0c1b5483d2e83bb3c732a787187be8227868068c
Sourjya_1614 <159236410+Sourjya-Saha@users.noreply.github.com>
2025-09-09 02:48:19 +0530

    Update layout.js

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -53,7 +53,7 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Clever Loo" />
       </head>
-      <body className={${poppins.className} bg-[#026738]}>
+      <body className={`${poppins.className} bg-[#026738]`}>
         <NextAuthSessionProvider>
           {children}
         </NextAuthSessionProvider>
